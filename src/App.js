0744cd2fde2e3e833cedf13da8b5c6408af6d89c
Md Salman <md.salman@alphasq.com>
2021-05-02 19:59:50 +0600

    Header Footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import {Container} from 'react-bootstrap';
 
 function App() {
   return (
     <div>
-      
+      <Header/>
+      <main className="py-3">
+        <Container>
+        <h1>Welcome to PROSHOP</h1>
+        </Container>
+      </main>
+      <Footer/>
     </div>
   );
 }
